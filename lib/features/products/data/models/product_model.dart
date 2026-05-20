8709bd93d413864b79dfe1e8d865135a293c139e
HaneenEhdaa <116807965+HaneenEhdaa@users.noreply.github.com>
2026-05-20 18:56:28 +0200

    create product model

---
+++ lib/features/products/data/models/product_model.dart
@@ -0,0 +1,77 @@
+class ProductModel {
+  final int id;
+  final String title;
+  final String description;
+  final String category;
+  final double price;
+  final double discountPercentage;
+  final double rating;
+  final List<String> tags;
+  final String brand;
+  final List<ReviewModel> reviews;
+  final List<String> images;
+  final String thumbnail;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
+    required this.tags,
+    required this.brand,
+    required this.reviews,
+    required this.images,
+    required this.thumbnail,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      category: json['category'],
+      price: (json['price'] as num).toDouble(),
+      discountPercentage: (json['discountPercentage'] as num).toDouble(),
+      rating: (json['rating'] as num).toDouble(),
+      tags: List<String>.from(json['tags']),
+      brand: json['brand'],
+      reviews: (json['reviews'] as List)
+          .map(
+            (review) => ReviewModel.fromJson(review),
+          )
+          .toList(),
+      images: List<String>.from(json['images']),
+      thumbnail: json['thumbnail'],
+    );
+  }
+}
+
+
+class ReviewModel {
+  final int rating;
+  final String comment;
+  final String date;
+  final String reviewerName;
+  final String reviewerEmail;
+
+  ReviewModel({
+    required this.rating,
+    required this.comment,
+    required this.date,
+    required this.reviewerName,
+    required this.reviewerEmail,
+  });
+
+  factory ReviewModel.fromJson(Map<String, dynamic> json) {
+    return ReviewModel(
+      rating: json['rating'],
+      comment: json['comment'],
+      date: json['date'],
+      reviewerName: json['reviewerName'],
+      reviewerEmail: json['reviewerEmail'],
+    );
+  }
+}
